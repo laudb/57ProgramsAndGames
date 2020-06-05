98724567827ab43c429721d414c7ce57e1ca0a6d
Laud Bentil <laudbentil@gmail.com>
2020-06-05 23:43:30 +0000

    add initial code and initial steps

---
+++ arithmetic-mean/arithmetic-mean.js
@@ -0,0 +1,13 @@
+// Arithmetic Mean
+
+// assign 0 to x and n
+let x , n = 0;
+
+// prompt + input sample
+
+// calculate sample nuber + current mean
+
+// output current mean
+
+// if input is not end, repeat step 2
+
