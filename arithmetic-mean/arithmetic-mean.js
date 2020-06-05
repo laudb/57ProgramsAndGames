69077c9bf12b52bcf6bd4e64a9080f7be7b1b24c
Laud Bentil <laudbentil@gmail.com>
2020-06-05 23:57:57 +0000

    add initial version of arithmetic mean

--- arithmetic-mean/arithmetic-mean.js
+++ arithmetic-mean/arithmetic-mean.js
@@ -1,9 +1,30 @@
 // Arithmetic Mean
 
+const readline = require('readline');
 // assign 0 to x and n
 let x , n = 0;
 
 // prompt + input sample
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('This program computes the arithmetic mean ')
+
+rl.question('Enter sample ', (sample) => {
+    
+    if (sample === 'end') {
+        console.log('Ending')
+        rl.close();
+    }
+
+    n = n + 1
+    x = x + sample
+
+})
+
+rl.on('close', () => console.log('End'))
 
 // calculate sample nuber + current mean
 
