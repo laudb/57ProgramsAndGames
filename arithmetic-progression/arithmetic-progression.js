d0924e6c71b23ad9381cd12305bead41bcdb2921
Laud Bentil <laudbentil@gmail.com>
2020-06-22 23:49:41 +0000

    fix minor bug

--- arithmetic-progression/arithmetic-progression.js
+++ arithmetic-progression/arithmetic-progression.js
@@ -3,7 +3,7 @@
 const readline = require('readline');
 const promisify = require('util');
 // const f, d, t, option = 0;
-const a, d, n, c = 0;
+let a = d = n = c = t = 0;
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,9 +11,9 @@ const rl = readline.createInterface({
 });
 
 rl.question("First term ", function( first ) {
-    rl.question("common difference", function( difference ) {
-        rl.question("number of terms", function ( terms ) {
-            r1.question("for table type 1, if not type 0", function ( table ) {
+    rl.question("common difference ", function( difference ) {
+        rl.question("number of terms ", function ( terms ) {
+            rl.question("for table type 1, if not type 0 ", function ( table ) {
 
                 if (table === 0) {
                     rl.close()
