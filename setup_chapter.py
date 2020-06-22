273f8d60a470c89af8cd3abba8eaff6e28d89b1d
Laud Bentil <laudbentil@gmail.com>
2020-06-22 23:32:51 +0000

    create new empty files, minor fixes and user feedback

--- setup_chapter.py
+++ setup_chapter.py
@@ -1,32 +1,34 @@
 #! python3
 
+'''
+This program creates a new folder and the needed files
+for each chapter of this project.
+'''
+
 # import the relevant packages
 import os, sys
+
 
 # setup project
 current_path = os.getcwd()
 current_folder = current_path.split('/')[-1]
-print('currently working from ', current_folder)
-
+print('Currently working from ', current_folder)
 
 if current_folder != '57ProgramsAndGames':
     print('Please navigate to root folder')
     sys.exit()
 
-
 if len(sys.argv) < 2:
     print('Please add name of the project while running this command')
     sys.exit()
+    
 
 # take the cleaned argument from the commandline.
 project_name = sys.argv[1]
+project_name = project_name.lower()
 print(project_name) 
 
-# format project name to lowercase text.
-project_name = project_name.lower()
-
-# create folder using the validated input.
-# # confirm that folder exists, then change into folder. 
+# check that folder exists first, create folder using the validated input.
 if os.path.exists(project_name):
     print('Project Folder already exisits')
     sys.exit()
@@ -34,15 +36,15 @@ else:
     os.mkdir(project_name)
 
 extensions = ['bas','md','js']
-# create the files in the folder with validated input: [PROJECT_NAME.BAS, project_name.js, EXAMPLE.md].
-# files.append(project_name, 'example')
-## ugly file creator
+
+# create the files in the folder with validated input: [PROJECT_NAME.BAS, EXAMPLE.md, project_name.js].
+## ugly file namer
 
 files = []
 
 for extension in extensions:
+    file = str(project_name)
     if extension == 'bas':
-        file = str(project_name)
         file = file.upper()
         extension = extension.upper()
         file = str(file)+'.'+str(extension)
@@ -54,7 +56,6 @@ for extension in extensions:
         file = str(file)+'.'+str(extension)
         files.append(file)
     elif extension == 'js':
-        file = str(project_name)
         file = file.lower()
         extension = extension.lower()
         file = str(file)+'.'+str(extension)
@@ -63,21 +64,14 @@ for extension in extensions:
         print('unhandled case', extension)
 
 
-# project_name = project_name.upper()
-print(files)
-
-
-
-# file_array.append()
-
-# # confirm that folder exists, then change into folder. 
-# if os.path.exists(project_name):
-#     pass
-
-
+# create files
 for file in files:
-    print(os.path.join(current_path, project_name, file))
+    file = os.path.join(current_path, project_name, file)
+    print(file)
+    file = open(file, 'w')
+    file.write('\n')
 
 # Give feedback to the user about number of files created and display files names.
-
-
+new_folder_path = os.path.join(current_path, project_name)
+new_files = os.listdir(new_folder_path)
+print(len(new_files),' files created ', new_files)
