51f4694e9599ab1bb68aac90c2e8a8e3afb33b15
Laud Bentil <laudbentil@gmail.com>
2020-06-23 00:16:23 +0000

    complete flow for input and response

--- arithmetic-progression/arithmetic-progression.js
+++ arithmetic-progression/arithmetic-progression.js
@@ -3,7 +3,7 @@
 const readline = require('readline');
 const promisify = require('util');
 // const f, d, t, option = 0;
-let a = d = n = c = t = 0;
+let a = d = n = c = t = i = j = k = l= 0;
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,8 +11,11 @@ const rl = readline.createInterface({
 });
 
 rl.question("First term ", function( first ) {
+
     rl.question("common difference ", function( difference ) {
+    
         rl.question("number of terms ", function ( terms ) {
+
             rl.question("for table type 1, if not type 0 ", function ( table ) {
 
                 if (table === 0) {
@@ -22,6 +25,15 @@ rl.question("First term ", function( first ) {
                 a = first;
                 d += difference;
                 t += terms;
+                console.log('ARITHMETIC PROGRESSION')
+                console.log('TERM NUMBER','----------','TERM VALUE')
+                for (let i = 0; i < terms.length; i++) {
+                    k = i + 1;
+                    l = first + (i * difference )
+                    j += l
+                    console.log(k,'                   ', l)
+                }
+                console.log('Sum = ', j);
 
             })
         })
